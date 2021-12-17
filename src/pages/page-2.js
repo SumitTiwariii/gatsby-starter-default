880f468c04ee3c20cdec4ba23cf22d39566a8dee
SumitTiwariii <96292726+SumitTiwariii@users.noreply.github.com>
2021-12-17 16:44:36 +0530

    Update page-2.js

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -8,8 +8,8 @@ const SecondPage = () => (
   <Layout>
     <Seo title="Page two" />
     <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <p>Welcome to page of mine 2</p>
+    <Link to="/">Go back to the homepage byee</Link>
   </Layout>
 )
 
